a6e40fb82fae0fb93901543103ae0d5222ee2c55
wusushi <1105108139@gm.kuas.edu.tw>
2017-10-14 16:04:18 +0800

    Add files via upload

---
+++ week42.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class week42{
+            public static void main(String[] args)throws IOException{
+                         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+                         System.out.print("請輸入範圍的最小數 : ");
+                         int min = Integer.parseInt(buf.readLine());
+                         System.out.print("\n請輸入範圍的最大數 : ");
+                         int max = Integer.parseInt(buf.readLine());
+                         System.out.print("\n請輸入要尋找的倍數的基值 : ");
+                         int base = Integer.parseInt(buf.readLine());
+                         System.out.print("\n"+base+"的倍數為 : ");
+                         for(;min<=max;min++){
+                                if(min%base==0)
+                                        System.out.print(" "+min); 
+                         }
+            }
+}
