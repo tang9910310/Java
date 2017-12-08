1854e3d6c011ba37de87caec6730c2107f3e01e0
wusushi <1105108139@gm.kuas.edu.tw>
2017-12-08 19:13:02 +0800

    Add files via upload

---
+++ Game_account1302.java
@@ -0,0 +1,44 @@
+class Game_account{
+	private String account;
+	private String password;
+	static int KeyID = 0;
+	private int UID;
+    
+	public Game_account() {
+		this(null, null);
+	}
+	public Game_account(String account, String password) {
+		setGame_account(account, password);
+	}
+	public Game_account(Game_account game) {
+		this(game.get_Account(), game.get_Password());
+	}
+	public void setGame_account(String account, String password) {
+		set_Account(account);
+		set_Password(password);
+	}
+	private void set_Account(String name) {
+		account = name;
+	}
+	private void set_Password(String name) {
+		password = name;
+	}
+	public String get_Account() {
+		return account;
+	}
+	public String get_Password() {
+		KeyID++;
+		return password;
+	}
+	public int get_KeyID() {
+		return KeyID;
+	}
+	public int get_UID() {
+		return UID = 1105108100 + KeyID;
+	}
+    public void showProfile(){
+    	System.out.println("您的帳號：" + account);
+    	System.out.println("您的密碼：" + password);
+    	System.out.println("您的識別碼：" + get_UID());
+    }
+}
---
+++ week1302.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class week1302 {
+  static int count = 0;
+  public static void main(String[] args) throws IOException {
+    String account, password1, password2;
+    BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+    Game_account game[] = new Game_account[++count];
+    System.out.print("請輸入你的帳號：");
+    account = buf.readLine();
+    do {
+    	System.out.print("請輸入你的密碼：");
+    	password1 = buf.readLine();
+    	System.out.print("請再次輸入你的密碼：");
+    	password2 = buf.readLine();
+    } while (!password1.equals(password2));
+    game[--count] = new Game_account(account, password1);
+    game[count].showProfile();
+  }
+}
